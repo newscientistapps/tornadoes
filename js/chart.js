d478503f35f321692cef3bd8744bbc7bd7a89df2
paldhous <peter@peteraldhous.com>
2013-05-21 14:32:18 -0700

    added legend, circle replaces marker for Moore

--- js/chart.js
+++ js/chart.js
@@ -48,7 +48,7 @@ $(function () {
                     text: 'Deaths per year',
                     style: {
 	                     color: '#000',
-	                     fontWeight: '400'
+	                     fontWeight: '800'
                     	}
                     },
                 max: 500,
