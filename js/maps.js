726869dea36db47c2c2a0a3f39b547bb975b247c
paldhous <peter@peteraldhous.com>
2013-05-22 08:23:28 -0700

    less zoom

--- js/maps.js
+++ js/maps.js
@@ -1,4 +1,4 @@
-var map = L.mapbox.map('map', 'newscientist26102012.map-1jbwssor', {maxZoom: 12, minZoom: 4})
+var map = L.mapbox.map('map', 'newscientist26102012.map-1jbwssor', {maxZoom: 9, minZoom: 4})
       .setView([38, -98], 5);
 
       
