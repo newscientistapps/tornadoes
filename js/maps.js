0f30c0d5327db6d2901cde704d6ac7d200e5db85
paldhous <peter@peteraldhous.com>
2013-05-22 08:20:03 -0700

    limit map zoom

--- js/maps.js
+++ js/maps.js
@@ -1,5 +1,6 @@
-var map = L.mapbox.map('map', 'newscientist26102012.map-1jbwssor')
+var map = L.mapbox.map('map', 'newscientist26102012.map-1jbwssor', {maxZoom: 12, minZoom: 4})
       .setView([38, -98], 5);
+
       
 
 
