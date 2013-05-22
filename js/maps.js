252a7456e86591b1b55f638526bc9d5b8642a69f
paldhous <peter@peteraldhous.com>
2013-05-22 07:29:17 -0700

    fixed zoom levels and chart tooltip

--- js/maps.js
+++ js/maps.js
@@ -1,5 +1,11 @@
-var map = L.mapbox.map('map', 'newscientist26102012.map-1jbwssor')
-      .setView([38, -98], 5);
+var tornado = L.mapbox.tileLayer('newscientist26102012.map-1jbwssor')
 
+var map = L.map('map', {
+    center: new L.LatLng(38, -98),
+    zoom: 5, 
+    maxZoom: 12,
+    minZoom: 4,
+    layers: [tornado] 
+});
 
 
